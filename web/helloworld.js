8318981b459e891285b64e239c836e8926b7fe13
arendu <adithya.r@gmail.com>
2015-05-25 21:23:46 -0400

    traversal of tree working...

--- web/helloworld.js
+++ web/helloworld.js
@@ -1,45 +1,131 @@
 /**
  * Created by arenduchintala on 5/20/15.
  */
+var rootPhraseNode;
 
-function add(num1, num2) {
-    return num1 + num2
-}
-function changetext(id) {
-    $(id).html($(id).html() + " " + $(id).html());
-
-}
-function getsent() {
-    return "a new sentence";
-    //for w in sent:
-    //    $('body'.append("<span id='id'+w >w</span>");
-}
 function ready() {
     var sentence = "this is a test sentence";
-    showsent(sentence);
-    /*var tableProto = Object.create(HTMLTableElement.prototype);
-
-
-     Object.defineProperty(tableProto, 'middleTxt', {
-     value: "middletxt", writable: true
-     });
-
-     var wordTable = document.registerElement('word-table', {
-     prototype: tableProto
-     });*/
-
-
+    var phraseTreeStr = "(S (NP I) (VP (V saw) (X him)))";
+    rootPhraseNode = parsePhraseTree(phraseTreeStr);
+    var phraseLeaves = getleaves(rootPhraseNode);
+    console.log("done...");
+    showsent(phraseLeaves);
 }
+function removeEmptyStrings(val) {
+    return !(val == "" || val == " ");
+}
+function getleaves(rootPhraseTree) {
+    var _stack = [];
+    var leaves = [];
+    var pn;
+    _stack.push(rootPhraseTree);
+    while (_stack.length > 0) {
+        pn = _stack.pop();
+        if (pn.phraseChildren.length == 0) {
+            leaves.push(pn);
+        } else {
+            for (var i = pn.phraseChildren.length - 1; i > -1; i--) {
+                var c = pn.phraseChildren[i];
+                _stack.push(c);
+            }
+        }
+    }
+    return leaves;
+}
+function parsePhraseTree(phraseTreeStr) {
+    //"1、2、3".split(/()/g) == ["1", "、", "2", "、", "3"]
+    var _phraseTreeList = phraseTreeStr.replace(/\s\s+/g, ' ').split(/(\(|\)|\s)/g).filter(removeEmptyStrings)
+    //console.log(_phraseTreeList);
+    _phraseTreeList.pop();
+    _phraseTreeList.shift(); //discard outter brackets
+    var _stack = [];
+    var item, nextItem, pn, toppn, rootpn;
+    nextItem = _phraseTreeList.shift()
+    pn = new PhraseNode(nextItem, null);
+    _stack.push(pn);
+
+    while (_stack.length > 0 && _phraseTreeList.length > 0) {
+        item = _phraseTreeList.shift()
+        if (item == "(") {
+            nextItem = _phraseTreeList.shift()
+            toppn = _stack[_stack.length - 1];
+            pn = new PhraseNode(nextItem, toppn);
+            toppn.addPhraseChild(pn);
+            _stack.push(pn);
+        } else if (item == ")") {
+            _stack.pop();
+            rootpn = _stack.pop();
+        } else {
+            toppn = _stack[_stack.length - 1];
+            pn = new PhraseNode(item, toppn);
+            toppn.addPhraseChild(pn);
+            _stack.push(pn);
+        }
+    }
+    return rootpn;
+}
+
 function spanClicked(e) {
     console.log("a span has been clicked:" + e.target.id);
     var tablenum = e.target.id.split(",")[1];
+    var wordTable = document.getElementById(tablenum);
+    var pn = wordTable.phraseNode;
     var rownum = parseInt(e.target.id.split(",")[2]);
     if (rownum == 0) {
-        merge(tablenum);
+        console.log("phrase: " + pn.phrase + "clicked, go up to parent");
+        goUpToParent(wordTable);
     } else {
-        split(tablenum);
+        var pn = wordTable.phraseNode;
+        console.log("phrase: " + pn.phrase + "clicked, go down to children")
+        goDonwToChildren(wordTable);
+
     }
     e.stopPropagation();
+}
+
+function goUpToParent(wordTable) {
+    var pn = wordTable.phraseNode;
+    pn.isMyAncestor(pn);
+    var parentPhraseNode = wordTable.phraseNode.parent;
+    if (parentPhraseNode != null) {
+        var containerDiv = wordTable.parentNode;
+        var parentWordTable = createWordTable(wordTable.id, parentPhraseNode);
+        containerDiv.insertBefore(parentWordTable, wordTable);
+        removeDescents(parentWordTable);
+    }
+    redoIds(containerDiv);
+}
+function removeDescents(wordTable) {
+    var containerDiv = wordTable.parentNode;
+    var setForRemoval = []
+    NodeList.prototype.forEach = Array.prototype.forEach
+    var children = containerDiv.childNodes;
+    children.forEach(function (item) {
+        if (item.phraseNode.isMyAncestor(wordTable.phraseNode)) {
+            setForRemoval.push(item);
+        }
+    });
+
+    for (var i = 0; i < setForRemoval.length; i++) {
+        var rem = setForRemoval[i];
+        console.log(rem.phraseNode.phrase + " is being removed");
+        containerDiv.removeChild(rem);
+    }
+}
+function goDonwToChildren(wordTable) {
+    if (wordTable.phraseNode.phraseChildren.length > 0) {
+        var currentid = parseInt(wordTable.id);
+        var containerDiv = wordTable.parentNode;
+        for (var i = 0; i < wordTable.phraseNode.phraseChildren.length; i++) {
+            var pn = wordTable.phraseNode.phraseChildren[i];
+            var cwt = createWordTable(currentid + i, pn);
+            containerDiv.insertBefore(cwt, wordTable);
+        }
+        containerDiv.removeChild(wordTable);
+        redoIds(containerDiv);
+    } else {
+        console.log("phrase node:" + wordTable.phraseNode.phrase + " has no children");
+    }
 }
 
 function split(tableNum) {
@@ -49,7 +135,7 @@ function split(tableNum) {
     var insertAfterTable = document.getElementById(insertAfterId);
     var currentTable = document.getElementById(tableNum);
     console.log("need to split " + currentTable.id);
-    var parentDiv = currentTable.parentNode;
+    var containerDiv = currentTable.parentNode;
 
     var txt2split = currentTable.middleTxt.split(" ");
     if (txt2split.length == 1) {
@@ -60,21 +146,21 @@ function split(tableNum) {
             //var elem = tableCreate(i, 3, 1, stringarr[i]);
             var elem1 = createWordTable(i + o, txt2split[o]);
 
-            parentDiv.insertBefore(elem1, currentTable);
+            containerDiv.insertBefore(elem1, currentTable);
         }
-        parentDiv.removeChild(currentTable);
-        redoIds(parentDiv);
+        containerDiv.removeChild(currentTable);
+        redoIds(containerDiv);
     }
 
 }
 
-function redoIds(parentDiv) {
+function redoIds(containerDiv) {
     NodeList.prototype.forEach = Array.prototype.forEach
-    var children = parentDiv.childNodes;
+    var children = containerDiv.childNodes;
     var i = 0;
     children.forEach(function (item) {
         item.setNewId(i);
-        item.setMiddleTxt(item.middleTxt);
+        item.setPhraseNode(item.phraseNode);//just for debugging
         i++;
     });
 }
@@ -89,41 +175,41 @@ function merge(tableNum) {
         console.log("need to merge " + currentTable.id + " and " + neighborTable.id);
         var newMiddleTxt = currentTable.middleTxt + " " + neighborTable.middleTxt;
         var mergedTable = createWordTable(tableNum, newMiddleTxt);
-        var parentDiv = currentTable.parentNode;
-        parentDiv.insertBefore(mergedTable, currentTable);
-        parentDiv.removeChild(currentTable);
-        parentDiv.removeChild(neighborTable);
-        redoIds(parentDiv);
+        var containerDiv = currentTable.parentNode;
+        containerDiv.insertBefore(mergedTable, currentTable);
+        containerDiv.removeChild(currentTable);
+        containerDiv.removeChild(neighborTable);
+        redoIds(containerDiv);
     }
 
 }
 
-function showsent(stringStr) {
+function showsent(phraseNodes) {
     var lineDiv = document.createElement("div");
     lineDiv.id = "myLineDiv";
     document.body.appendChild(lineDiv);
-    var stringarr = stringStr.split(" ");
-    for (var i = 0; i < stringarr.length; i++) {
+
+    for (var i = 0; i < phraseNodes.length; i++) {
+        var pn = phraseNodes[i];
         //var elem = tableCreate(i, 3, 1, stringarr[i]);
-        var elem = createWordTable(i, stringarr[i]);
+        var elem = createWordTable(i, pn);
         lineDiv.appendChild(elem);
     }
 }
 
 
-function createWordTable(numid, txt) {
+function createWordTable(numid, phraseNode) {
     wordTable = document.createElement("table");
     wordTable.id = numid.toString();
-    wordTable.middleTxt = txt;
+    wordTable.phraseNode = phraseNode;
     wordTable.style.display = "inline-block";
     wordTable.style.float = "left";
     for (var i = 0; i < 3; i++) {
         var tr = wordTable.insertRow();
         for (var j = 0; j < 1; j++) {
-
             var td = tr.insertCell();
             if (i == 1) {
-                td.innerHTML = wordTable.middleTxt + "," + wordTable.id;
+                td.innerHTML = wordTable.phraseNode.phrase + "," + wordTable.id;
             } else {
                 td.appendChild(document.createTextNode(""));
                 td.id = "cell," + numid.toString() + "," + i.toString();
@@ -138,9 +224,9 @@ function createWordTable(numid, txt) {
         }
     }
 
-    wordTable.setMiddleTxt = function (newmiddleTxt) {
-        this.middleTxt = newmiddleTxt;
-        this.rows[1].cells[0].innerHTML = this.middleTxt + "," + this.id;
+    wordTable.setPhraseNode = function (newPhraseNode) {
+        this.phraseNode = newPhraseNode;
+        this.rows[1].cells[0].innerHTML = this.phraseNode.phrase + "," + this.id;
     }
 
     wordTable.setNewId = function (newId) {
@@ -150,5 +236,31 @@ function createWordTable(numid, txt) {
     }
 
     return wordTable;
+
+}
+
+function PhraseNode(phrase, parent) {
+    this.phrase = phrase;
+    this.parent = parent;
+    this.phraseChildren = [];
+    this.addPhraseChild = function (phraseNode) {
+        this.phraseChildren.push(phraseNode);
+    }
+
+    this.toString = function () {
+        return "PhraseNode:" + this.phrase;
+    }
+    this.isMyAncestor = function (pn) {
+        var isAncestor = false
+        var a_parent = this.parent;
+        while (a_parent != null) {
+            console.log("ancestor:" + a_parent.phrase + (a_parent == pn));
+            if (a_parent == pn) {
+                isAncestor = true;
+            }
+            a_parent = a_parent.parent;
+        }
+        return isAncestor;
+    }
 
 }
