d419ef199c3d12b1095e15f8646b0024ad0b382f
arendu <adithya.r@gmail.com>
2015-10-09 00:59:44 -0400

    correct names of fields

--- js/messages.js
+++ js/messages.js
@@ -1,6 +1,6 @@
 function SocketMessage(user, rule, before, after) {
-	this.user = user
+	this.username = user
 	this.rule = rule
-	this.before = before
-	this.after = after
+	this.state_before = before
+	this.state_after = after
 }
